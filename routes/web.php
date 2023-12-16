08450a2f91d1a165cd8395f12fac639c3f6e43eb
Anastasia Fomenko <anastafo@bk.ru>
2023-12-16 20:35:10 +0300

    add admin panel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use TCG\Voyager\Facades\Voyager;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
 });
